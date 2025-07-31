6e7d79ba5399e2509f61480b50a70b73d610997f
Piyush Singh <piyush@gmail.com>
2025-07-31 17:53:52 +0530

    Project completed

---
+++ server/routes/aiRoutes.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { generateArticle, generateBlogArticle, generateImage, removeImageBackground, removeImageObject, resumeReview } from '../controllers/aiController.js'
+import { auth } from '../middleware/auth.js'
+import { upload } from '../config/multer.js'
+
+const aiRouter = express.Router()
+
+aiRouter.post('/generate-article', auth, generateArticle)
+
+aiRouter.post('/generate-blog-title', auth, generateBlogArticle)
+
+aiRouter.post('/generate-image', auth, generateImage)
+
+aiRouter.post('/remove-image-background', upload.single('image'), auth, removeImageBackground)
+
+aiRouter.post('/remove-image-object', upload.single('image'), auth, removeImageObject)
+
+aiRouter.post('/resume-review', upload.single('resume'), auth, resumeReview)
+
+
+
+export default aiRouter;
